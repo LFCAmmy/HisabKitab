9681671518db4a2cd0547bb7518d4b9d926cd8a7
Attikz <pratikzhudson@gmail.com>
2018-09-06 15:05:35 +0545

    Created a login activty

---
+++ app/src/main/java/susankyatech/com/hisabkitab/LoginActivity.java
@@ -0,0 +1,16 @@
+package susankyatech.com.hisabkitab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.contain_login_frame,new LoginFragment()).commit();
+
+    }
+}

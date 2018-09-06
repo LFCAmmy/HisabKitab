c7b72351227fed356c629da601e7baf18d0026fe
lfcammy <armaan_s@hotmail.co.uk>
2018-09-06 14:59:23 +0545

    Welcome Activity

---
+++ app/src/main/java/susankyatech/com/hisabkitab/Fragment/GettingStartedFragment.java
@@ -0,0 +1,77 @@
+package susankyatech.com.hisabkitab.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import susankyatech.com.hisabkitab.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GettingStartedFragment extends Fragment {
+
+    @BindView(R.id.group_code)
+    EditText groupCode;
+    @BindView(R.id.join_group_btn)
+    Button joinGroup;
+    @BindView(R.id.create_group_tv)
+    TextView createGroup;
+
+    public GettingStartedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_getting_started, container, false);
+
+        ButterKnife.bind(this, view);
+
+        init();
+
+        return view;
+    }
+
+    private void init() {
+        joinGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = groupCode.getText().toString();
+
+                if (TextUtils.isEmpty(code)){
+                    
+                }
+            }
+        });
+
+
+        createGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new CreateGroupFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.content_welcome_frame, fragment);
+                transaction.addToBackStack(null);
+
+                transaction.commit();
+            }
+        });
+    }
+
+}

3f49501b0386f4afecf64ecec37f76f5f9a82968
Attikz <pratikzhudson@gmail.com>
2018-09-18 13:20:38 +0545

    - Optimized

--- app/src/main/java/susankyatech/com/hisabkitab/GroupExpensesFragment.java
+++ app/src/main/java/susankyatech/com/hisabkitab/GroupExpensesFragment.java
@@ -4,29 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
 import java.util.List;
 
-import static android.support.constraint.Constraints.TAG;
-
 public class GroupExpensesFragment extends Fragment {
 
-    private RecyclerView recyclerView;
-
-    private Query userReference;
+    private DatabaseReference userReference;
 
     private String currentUserId;
 
@@ -38,13 +32,11 @@ public class GroupExpensesFragment extends Fragment {
         View mView = inflater.inflate(R.layout.fragment_group_expenses, container, false);
 
         Button clearDueBtn = mView.findViewById(R.id.group_expenses_clear_due_btn);
-        recyclerView = mView.findViewById(R.id.group_expenses_recycler_view);
+        RecyclerView recyclerView = mView.findViewById(R.id.group_expenses_recycler_view);
 
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         currentUserId = mAuth.getCurrentUser().getUid();
         userReference = FirebaseDatabase.getInstance().getReference().child("Users");
-
-        displayUserList();
 
         clearDueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,38 +48,30 @@ public class GroupExpensesFragment extends Fragment {
         return mView;
     }
 
-    private void displayUserList() {
+    @Override
+    public void onStart() {
+        super.onStart();
 
-
-        FirebaseRecyclerAdapter<UserDataModel, UserListHolder> adapter = new FirebaseRecyclerAdapter<UserDataModel, UserListHolder>
-                (UserDataModel.class, R.layout.group_expenses_recycler_view_layout, UserListHolder.class, userReference) {
-
+        FirebaseRecyclerAdapter<UserDataModel, UserDataViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<UserDataModel, UserDataViewHolder>
+                (UserDataViewHolder.class, R.layout.manage_groups_update_group_members_recycler_view_layout, UserDataModel.class) {
             @Override
-            protected void populateViewHolder(UserListHolder viewHolder, UserDataModel model, int position) {
+            protected void onBindViewHolder(@NonNull UserDataViewHolder holder, int position, @NonNull UserDataModel model) {
 
-                Log.d("TAG", "populateViewHolder: " + model.toString());
-                viewHolder.setUser_name(model.getUser_name());
+            }
+
+            @NonNull
+            @Override
+            public UserDataViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                return null;
             }
         };
-
-        recyclerView.setAdapter(adapter);
     }
 
-    public static class UserListHolder extends RecyclerView.ViewHolder {
+    public static class UserDataViewHolder extends RecyclerView.ViewHolder {
 
-        View view;
 
-        public UserListHolder(@NonNull View itemView) {
+        public UserDataViewHolder(@NonNull View itemView) {
             super(itemView);
-            view = itemView;
-        }
-
-        public void setUser_name(String user_name) {
-            Log.d("qwerty", "masd" + user_name);
-
-            TextView name = view.findViewById(R.id.group_expenses_user_name_tv);
-            name.setText(user_name);
-
         }
     }
 }

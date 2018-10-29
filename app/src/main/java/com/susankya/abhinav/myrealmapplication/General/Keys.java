86e9647b14bd6e2e15cfbe0e12a95ebde51b8a40
Attikz <pratikzhudson@gmail.com>
2018-10-29 10:41:45 +0545

    --

---
+++ app/src/main/java/com/susankya/abhinav/myrealmapplication/General/Keys.java
@@ -0,0 +1,10 @@
+package com.susankya.abhinav.myrealmapplication.General;
+
+public interface Keys {
+
+    String PREFERENCES = "UserInfo";
+    String ROLE = "role";
+    String IS_LOGGED_IN = "loggedIn";
+    String GROUP_ID = "groupId";
+}
+
